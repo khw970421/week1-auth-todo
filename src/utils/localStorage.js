fb191ceb798cd6f89805c251b5a22f84fc80f260
khw970421 <khw970421@kakao.com>
2022-12-25 14:37:50 +0900

    chec

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -2,11 +2,10 @@ const setItem = (key, value) =>
   localStorage.setItem(key, JSON.stringify(value));
 
 const getItem = (key) => JSON.parse(localStorage.getItem(key));
-console.log('!!!!!');
-if(1==1){
-  console.log('~~~~')
-}
+console.log("!!!!!")
 
+
+console.log("double")
 
 
 
