d66af5f33eeae2900d3e94eebf7bd40428396153
khw970421 <khw970421@kakao.com>
2022-12-25 14:30:19 +0900

    check

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -2,7 +2,11 @@ const setItem = (key, value) =>
   localStorage.setItem(key, JSON.stringify(value));
 
 const getItem = (key) => JSON.parse(localStorage.getItem(key));
-console.log('!!!!!')
+            console.log('!!!!!');
+
+
+
+            
 
 
 
